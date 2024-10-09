367d1a9490f84f20d8152a830e025149044121e1
Thang <nguyenthangnnh@outlook.com>
2024-10-09 12:25:49 +0700

    Add FireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -25,20 +26,22 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8 // Đã sửa
+        targetCompatibility = JavaVersion.VERSION_1_8 // Đã sửa
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -47,55 +50,52 @@ dependencies {
     implementation(libs.androidx.annotation)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.firebase.database)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-// Thư viện hình ảnh hình tròn
+    // Thư viện hình ảnh hình tròn
     implementation("de.hdodenhof:circleimageview:3.1.0")
-// Thư viện trình chiếu hình ảnh
+    // Thư viện trình chiếu hình ảnh
     implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
 
-// Thư viện ConstraintLayout
+    // Thư viện ConstraintLayout
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-// ViewModel và LiveData
+    // ViewModel và LiveData
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
 
-// Retrofit và OkHttp
+    // Retrofit và OkHttp
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-// Thư viện Glide
+    // Thư viện Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
-//    kapt ("com.github.bumptech.glide:compiler:4.12.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0") // Đã sửa
     implementation("jp.wasabeef:glide-transformations:4.3.0")
 
-// Google Maps
+    // Google Maps
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.libraries.places:places:4.0.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 
-// ViewPager2
-    implementation("androidx.fragment:fragment-ktx:1.8.3")
+    // ViewPager2
+    implementation("androidx.fragment:fragment-ktx:1.8.4")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("com.google.android.material:material:1.12.0")
 
-
-// Thư viện OTP
+    // Thư viện OTP
     implementation("com.github.aabhasr1:OtpView:v1.1.2-ktx")
 
-// Stripe
-    implementation("com.stripe:stripe-android:20.51.0")
+    // Stripe
+    implementation("com.stripe:stripe-android:20.52.0")
 
-
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.1") // Kiểm tra phiên bản mới nhất
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.1") // Kiểm tra phiên bản mới nhất
-
-
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.2") // Kiểm tra phiên bản mới nhất
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.2") // Kiểm tra phiên bản mới nhất
 }
